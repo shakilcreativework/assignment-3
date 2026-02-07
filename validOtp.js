1b6c5512be5f2eb9d8c870bed0e1bab2cd6d4332
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-07 22:32:53 +0600

    add all valid conditions for valid OPT

---
+++ validOtp.js
@@ -0,0 +1,18 @@
+/*function signature/sample */
+function validOtp(otp) {
+    // You have to write your code here
+    if(typeof otp !== 'string' || !otp.startsWith('ph-')){
+        return false;
+    }else if(!otp.length >= 8 || otp.length < 8){
+        return false;
+    }else if(otp.length === 8){
+        return true;
+    };
+}
+
+const getOtp = validOtp("ph-10985");
+// const getOtp = validOtp("ph-29456");
+// const getOtp = validOtp("dh-10985");
+// const getOtp = validOtp(12310985);
+// const getOtp = validOtp(["ph-10985"]);
+// console.log(getOtp);
