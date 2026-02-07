3cfef800404f3be1bfcb2aa3b8aa079206d683a4
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-07 22:05:23 +0600

    Fixed to conditons

--- newPrice.js
+++ newPrice.js
@@ -1,19 +1,16 @@
 /*function signature/sample */
 function newPrice(currentPrice , discount ) {
     // You have to write your code here
-    if(currentPrice === 0){
-        return 'Please, input real product price of Amount';
-    }else if(currentPrice < 0 || !currentPrice){
-        return 'Please, check invalid input product price of Amount';
-    }else if(typeof(currentPrice) === 'string' || typeof(discount) === 'string'){
-        return 'Please give me correct numbers of product amounts not string';
+    if(typeof currentPrice !== 'number' || typeof discount !== 'number'){
+        return 'Invalid, Please give me price of numbers.';
+    }else if(currentPrice <= 0){
+        return 'Invalid, Please give me real price of products';
     }else if(discount < 0 || discount > 100){
-        return 'Invalid amount Please, use valid discount amount greater then 0 and maximum 100'
+        return 'Invalid, discount';
     }else{
-        const amount = (currentPrice * discount) / 100;
-        const total = currentPrice - amount;
-        const payAmount = total.toFixed(3);
-        return payAmount;
+        const amoount = (currentPrice * discount) / 100;
+        const total = (currentPrice - amoount).toFixed(3);
+        return total;
     }
 }
 
@@ -24,6 +21,7 @@ const result = newPrice(1500, 20);
 // const result = newPrice("1000", 10);
 // const result = newPrice("0", 10);
 // const result = newPrice("0", 10);
+// const result = newPrice(1500, -101);
 // const result = newPrice(1500, 101);
 // const result = newPrice(2000, 10);
 
